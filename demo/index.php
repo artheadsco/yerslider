48ebea2a5cc5290dc2634da1e9e453fb71db49e2
johannheyne <mail@johannheyne.de>
2014-06-23 20:04:56 +0200

    [FIX] README Detaching

--- demo/index.php
+++ demo/index.php
@@ -26,7 +26,7 @@
 				YerSlider is designed <strong>for developpers</strong>, <strong>high responsive</strong> and potentially <strong>can slide anything</strong>.</p>
 			<p class="alert">This page is casually under construction.</p>
             
-            <a class="download_btn" href="https://github.com/johannheyne/yerslider/archive/master.zip" target="_self"  title="">Download <span class="version">Version 1.4.4</span></a>
+            <a class="download_btn" href="https://github.com/johannheyne/yerslider/archive/master.zip" target="_self"  title="">Download <span class="version">Version 1.5.0</span></a>
             
             
 		</div>
@@ -533,6 +533,11 @@ myslider.init({
 		</div>
 
 		<ul class="changelog">
+			<li>Version 1.5.0 <span class="date">2014-06-23</span>
+                <ul>
+                	<li>NEW Detachings, take some slide content and put it elsewhere.</li>
+                </ul>
+            </li>
 			<li>Version 1.4.4 <span class="date">2014-05-27</span>
                 <ul>
                 	<li>The YerSliderGlobals object can adapt options for different enviroments.</li>
